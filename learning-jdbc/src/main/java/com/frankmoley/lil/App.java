c920d10b6d1c7399dccb992c25dfc06df3f0906a
zhyd1997 <zhyd007@gmail.com>
2025-04-13 16:34:50 +0800

    update

--- learning-jdbc/src/main/java/com/frankmoley/lil/App.java
+++ learning-jdbc/src/main/java/com/frankmoley/lil/App.java
@@ -1,10 +1,41 @@
 package com.frankmoley.lil;
 
+import com.frankmoley.lil.data.dao.ServiceDao;
+import com.frankmoley.lil.data.entity.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ServiceDao serviceDao = new ServiceDao();
+
+        List<Service> services = serviceDao.getAll();
+        System.out.println("**** SERVICES ****");
+        System.out.println("\n*** GET ALL ***");
+        services.forEach(System.out::println);
+
+        Optional<Service> service = serviceDao.getOne(services.get(0).getServiceId());
+        System.out.println("\n*** GET ONE ***\n");
+        System.out.println(service.get());
+
+        Service newService = new Service();
+        newService.setName("FooBarBaz" + System.currentTimeMillis());
+        newService.setPrice(new BigDecimal(10.35));
+        Service createdService= serviceDao.create(newService);
+        System.out.println("\n*** CREATE ***\n");
+        System.out.println(createdService);
+
+        createdService.setPrice(new BigDecimal(8.35));
+        Service updatedService= serviceDao.update(createdService);
+        System.out.println("\n*** UPDATE ***\n");
+        System.out.println(updatedService);
+
+        serviceDao.delete(updatedService.getServiceId());
+        System.out.println("\n*** DELETE ***\n");
     }
 }
--- learning-jdbc/src/main/java/com/frankmoley/lil/data/dao/ServiceDao.java
+++ learning-jdbc/src/main/java/com/frankmoley/lil/data/dao/ServiceDao.java
@@ -1,45 +1,128 @@
 package com.frankmoley.lil.data.dao;
 
 import com.frankmoley.lil.data.entity.Service;
+import com.frankmoley.lil.data.util.DatabaseUtils;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 public class ServiceDao implements Dao<Service, UUID> {
+    private static final Logger LOGGER = Logger.getLogger(ServiceDao.class.getName());
+    private static final String GET_ALL = "select * from wisdom.services";
+    private static final String GET_BY_ID = "select * from wisdom.services where service_id = ?";
+    private static final String CREATE = "insert into wisdom.services (service_id, name, price) values (?, ?, ?)";
+    private static final String UPDATE = "update wisdom.services set name = ?, price = ? where service_id = ?";
+    private static final String DELETE = "delete from wisdom.services where service_id = ?";
+
     @Override
     public List<Service> getAll() {
-        return List.of();
+        List<Service> services = new ArrayList<>();
+        Connection connection = DatabaseUtils.getConnection();
+        try(Statement statement = connection.createStatement()){
+            ResultSet rs = statement.executeQuery(GET_ALL);
+            services = this.processResultSet(rs);
+        } catch (SQLException e) {
+            DatabaseUtils.handleSqlException("ServiceDao.getAll", e, LOGGER);
+        }
+        return services;
     }
 
     @Override
     public Service create(Service entity) {
-        return null;
+        UUID serviceId = UUID.randomUUID();
+        Connection connection = DatabaseUtils.getConnection();
+        try{
+            connection.setAutoCommit(false);
+            PreparedStatement statement = connection.prepareStatement(CREATE);
+            statement.setObject(1, serviceId);
+            statement.setString(2, entity.getName());
+            statement.setBigDecimal(3, entity.getPrice());
+            statement.execute();
+            connection.commit();
+            statement.close();
+        }catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException sqle) {
+                DatabaseUtils.handleSqlException("ServiceDao.create.rollback", sqle, LOGGER);
+            }
+            DatabaseUtils.handleSqlException("ServiceDao.create", e, LOGGER);
+        }
+        Optional<Service> service = this.getOne(serviceId);
+        if (!service.isPresent()) {
+            return null;
+        }
+        return service.get();
     }
 
     @Override
-    public Optional<Service> getOne(UUID uuid) {
+    public Optional<Service> getOne(UUID id) {
+        try (PreparedStatement statement = DatabaseUtils.getConnection().prepareStatement(GET_BY_ID)) {
+            statement.setObject(1, id);
+            ResultSet rs = statement.executeQuery();
+            List<Service> services = this.processResultSet(rs);
+            if (services.isEmpty()) {
+                return Optional.empty();
+            }
+            return Optional.of(services.get(0));
+        } catch (SQLException e) {
+            DatabaseUtils.handleSqlException("ServiceDao.getOne", e, LOGGER);
+        }
         return Optional.empty();
     }
 
     @Override
     public Service update(Service entity) {
-        return null;
+        Connection connection = DatabaseUtils.getConnection();
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement statement = connection.prepareStatement(UPDATE);
+            statement.setString(1, entity.getName());
+            statement.setBigDecimal(2, entity.getPrice());
+            statement.setObject(3, entity.getServiceId());
+            statement.execute();
+            connection.commit();
+            statement.close();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException sqle) {
+                DatabaseUtils.handleSqlException("ServiceDao.update.rollback", sqle, LOGGER);
+            }
+            DatabaseUtils.handleSqlException("ServiceDao.update", e, LOGGER);
+        }
+        return this.getOne(entity.getServiceId()).get();
     }
 
     @Override
-    public void delete(UUID uuid) {
-
+    public void delete(UUID id) {
+        Connection connection = DatabaseUtils.getConnection();
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement statement = connection.prepareStatement(DELETE);
+            statement.setObject(1, id);
+            statement.execute();
+            connection.commit();
+            statement.close();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException sqle) {
+                DatabaseUtils.handleSqlException("ServiceDao.delete.rollback", sqle, LOGGER);
+            }
+            DatabaseUtils.handleSqlException("ServiceDao.delete", e, LOGGER);
+        }
     }
 
     private List<Service> processResultSet(ResultSet rs) throws SQLException {
         List<Service> services = new ArrayList<>();
         while (rs.next()) {
             Service service = new Service();
-            service.setServiceId((UUID) rs.getObject("serviceId"));
+            service.setServiceId((UUID) rs.getObject("service_id"));
             service.setName(rs.getString("name"));
             service.setPrice(rs.getBigDecimal("price"));
             services.add(service);
